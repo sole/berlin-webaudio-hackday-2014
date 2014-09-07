d22e6d2bb7f87be0695300a139c31123b9ef2a99
sole <sole@mozilla.com>
2014-09-08 00:10:13 +0100

    include envelopes example in slides

--- src/examples/envelopes/js/main.js
+++ src/examples/envelopes/js/main.js
@@ -13,6 +13,8 @@ window.addEventListener('load', function() {
 
   function onKeyDown(ev) {
 
+    ev.preventDefault();
+
     if(ev.keyCode !== 32 || playing) { // space
       // we don't care
       return;
@@ -44,11 +46,18 @@ window.addEventListener('load', function() {
 
   function setADSRListener(parameter) {
     var input = document.getElementById(parameter);
+    var valueLabel = document.createElement('span');
+    input.parentNode.appendChild(valueLabel);
+    
     input.step = 0.1;
     input.value = voice[parameter];
+    valueLabel.innerHTML = voice[parameter];
+    
     input.addEventListener('input', function() {
       voice[parameter] = this.value * 1.0;
+      valueLabel.innerHTML = this.value;
     });
+
   }
 
 });
